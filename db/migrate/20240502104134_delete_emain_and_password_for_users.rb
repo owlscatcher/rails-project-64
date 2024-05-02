61f442c9d4421c778cd8e1cb0d979b14f4d9eeac
Alexander Veselov <collabsis.g@gmail.com>
2024-05-02 15:37:19 +0400

    added devise

---
+++ db/migrate/20240502104134_delete_emain_and_password_for_users.rb
@@ -0,0 +1,6 @@
+class DeleteEmainAndPasswordForUsers < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :users, :email
+    remove_column :users, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_090047) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_02_105828) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -24,10 +24,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_090047) do
     t.string "username"
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "posts", "users"
