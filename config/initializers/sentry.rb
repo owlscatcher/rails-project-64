35a45cc72b643414eee24499a1d38b1ffca916cf
Alexander Veselov <collabsis.g@gmail.com>
2024-05-19 19:27:07 +0400

    on sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,17 +1,17 @@
-# Sentry.init do |config|
-#   config.dsn = 'https://d08b787602417aee3d69e77243a51a2b@o4507184816979968.ingest.de.sentry.io/4507184830349392'
-#   config.breadcrumbs_logger = %i[active_support_logger http_logger]
+Sentry.init do |config|
+  config.dsn = 'https://d08b787602417aee3d69e77243a51a2b@o4507184816979968.ingest.de.sentry.io/4507184830349392'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-#   # Set traces_sample_rate to 1.0 to capture 100%
-#   # of transactions for performance monitoring.
-#   # We recommend adjusting this value in production.
-#   config.traces_sample_rate = 1.0
-#   # or
-#   config.traces_sampler = lambda do |_context|
-#     true
-#   end
-#   # Set profiles_sample_rate to profile 100%
-#   # of sampled transactions.
-#   # We recommend adjusting this value in production.
-#   config.profiles_sample_rate = 1.0
-# end
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |_context|
+    true
+  end
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 1.0
+end
