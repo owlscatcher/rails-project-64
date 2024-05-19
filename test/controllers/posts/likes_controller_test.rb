0362bcb1a63d0974de45b5ceaaf2676568490998
Alexander Veselov <collabsis.g@gmail.com>
2024-05-19 19:12:33 +0400

    added test for like

--- test/controllers/posts/likes_controller_test.rb
+++ test/controllers/posts/likes_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class Posts::LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    sign_in @user
+
+    @post = posts(:two)
+    @like = post_likes(:one)
+  end
+
+  test 'should create a likes' do
+    assert_difference -> { PostLike.count } do
+      post post_likes_path(@post), params: { like: @like }
+    end
+
+    assert_redirected_to @post
+  end
+
+  test 'should destroy a like' do
+    like = PostLike.create!(user: @user, post: @post)
+
+    assert_difference -> { PostLike.count }, -1 do
+      delete post_like_path(@post, like)
+    end
+    assert_redirected_to @post
+  end
 end
